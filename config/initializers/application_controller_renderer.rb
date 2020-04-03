8511f849576f5043307d4ba8622e027a5763ae4e
yuuka-lav <60505747+yuuka-lav@users.noreply.github.com>
2020-04-03 18:07:36 +0900

    add function

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '3d1fd53db487936d13a376a961b15df41ee9fbc1cb2a124f01f44ae5c62ff266da34688db29c9693c90c0a9fb9a5e09e89236fe318d1a2d21fb0abc7edbf5b52'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'post_images#index'
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :post_comments, only: [:create]
+    resource :favorites, only: [:create, :destroy]
+end
+  resources :users, only: [:show, :edit, :update]
 end
